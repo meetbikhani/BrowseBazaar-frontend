4f26c6ff5429f99ddb04614a78ce14daee0f16e3
Meet Bikhani <146370378+meetbikhani@users.noreply.github.com>
2024-04-20 13:57:56 +0530

    Update Related.jsx

--- src/Related.jsx
+++ src/Related.jsx
@@ -12,45 +12,17 @@ import Loader from "./Loader";
 import { clearrelated, setloader } from "./Features/RelatedSlice";
 import { asyncgetrelated } from "./actions/RelatedActions";
 import { Toaster } from "react-hot-toast";
+import  { useRef } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import "./productslider.css"
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation  } from 'swiper/modules';
+import './slidersample.css';
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 40000000000000000, min: 3050 },
-    items: 10,
-  },
-  desktop: {
-    breakpoint: { max: 3050, min: 2500 },
-    items: 8,
-  },
-  desktop2: {
-    breakpoint: { max: 2500, min: 2145 },
-    items: 7,
-  },
-  desktop3: {
-    breakpoint: { max: 2145, min: 1860 },
-    items: 6,
-  },
-  desktop4: {
-    breakpoint: { max: 1860, min: 1520 },
-    items: 5,
-  },
-  desktop5: {
-    breakpoint: { max: 1520, min: 1200 },
-    items: 4,
-  },
-  desktop6: {
-    breakpoint: { max: 1200, min: 900 },
-    items: 3,
-  },
-  desktop7: {
-    breakpoint: { max: 900, min: 600 },
-    items: 2,
-  },
-  desktop8: {
-    breakpoint: { max: 600, min: 0 },
-    items: 1,
-  },
-};
 
 const Related = ({ productId, CatId, subCatId }) => {
   const data = useSelector((state) => state.related.Related);
@@ -68,69 +40,72 @@ const Related = ({ productId, CatId, subCatId }) => {
 
   return (
     <>
-      {loader ? (
-        <div className="flex items-center justify-center my-32">
-          <Loader />
-        </div>
-      ) : (
-        <Carousel
-          swipeable={true}
-          // centerMode={true}
-          draggable={true}
-          responsive={responsive}
-          className="mt-10"
-          arrows={false}
-          customButtonGroup={<CustomButtonGroup />}
-          additionalTransfrom={0}
-          autoPlay={true}
-          autoPlaySpeed={2000}
-          centerMode={false}
-          containerClass="container-with-dots"
-          dotListClass=""
-          focusOnSelect={false}
-          infinite
-          itemClass="mr-5"
-          keyBoardControl
-          minimumTouchDrag={80}
-          pauseOnHover
-          renderArrowsWhenDisabled={false}
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots={false}
-          sliderClass=""
-          slidesToSlide={1}
-        >
-          {data.map((item) => {
-            return (
-              <Link className="" key={item?.id} to={`/product/${item?.id}`}>
-                <div className="overflow-none productSliderCard">
-                  <Card id={item?.id} data={item} />
-                </div>
-              </Link>
-            );
-          })}
-        </Carousel>
-      )}
-      <Toaster
-        toastOptions={{
-          success: {
-            style: {
-              background: "green",
-              color: "white",
-            },
+    {/* <h1 className="mt-10 text-4xl ml-5">{title}</h1> */}
+    {loader ? (
+      <div className="flex h-full w-full items-center justify-center my-40">
+        <Loader />
+      </div>
+    ) : (
+      <Swiper 
+        slidesPerView={3}
+        spaceBetween={30}
+        loop={true}
+        breakpoints={{
+          '@0.00': {
+            slidesPerView: 1,
+            spaceBetween: 10,
           },
-          error: {
-            style: {
-              background: "red",
-            },
+          '@0.75': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          '@1.00': {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          '@1.50': {
+            slidesPerView: 4,
+            spaceBetween: 50,
           },
         }}
-      />
-    </>
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, Navigation]}
+        navigation={true}
+        className="mySwiper"
+      >
+        {data.map((item) => {
+          return (
+            
+              <SwiperSlide>
+              <Link key={item?.id} className="" to={`/product/${item?.id}`}>
+                <Card id={item?.id} data={item} />
+                </Link>
+              </SwiperSlide>
+            
+          );
+        })}
+      </Swiper>
+    )}
+    <Toaster
+      toastOptions={{
+        success: {
+          style: {
+            background: "green",
+            color: "white",
+          },
+        },
+        error: {
+          style: {
+            color: "white",
+            background: "red",
+          },
+        },
+      }}
+    />
+  </>
   );
 };
 
