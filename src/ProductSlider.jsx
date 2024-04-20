aa86a6493afc8732b43f94c780a7b462517260b3
Meet Bikhani <146370378+meetbikhani@users.noreply.github.com>
2024-04-20 13:56:50 +0530

    Update ProductSlider.jsx

--- src/ProductSlider.jsx
+++ src/ProductSlider.jsx
@@ -10,45 +10,20 @@ import { useSelector, useDispatch } from "react-redux";
 import { asyncgetproducts } from "./actions/ProductActions";
 import Loader from "./Loader";
 import toast, { Toaster } from "react-hot-toast";
+import  { useRef } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import "./productslider.css"
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 40000000000000000, min: 3050 },
-    items: 10,
-  },
-  desktop: {
-    breakpoint: { max: 3050, min: 2500 },
-    items: 8,
-  },
-  desktop2: {
-    breakpoint: { max: 2500, min: 2145 },
-    items: 7,
-  },
-  desktop3: {
-    breakpoint: { max: 2145, min: 1860 },
-    items: 6,
-  },
-  desktop4: {
-    breakpoint: { max: 1860, min: 1520 },
-    items: 5,
-  },
-  desktop5: {
-    breakpoint: { max: 1520, min: 1200 },
-    items: 4,
-  },
-  desktop6: {
-    breakpoint: { max: 1200, min: 900 },
-    items: 3,
-  },
-  desktop7: {
-    breakpoint: { max: 900, min: 600 },
-    items: 2,
-  },
-  desktop8: {
-    breakpoint: { max: 600, min: 0 },
-    items: 1,
-  },
-};
+import './slidersample.css';
+
+// import required modules
+import { Autoplay, Navigation  } from 'swiper/modules';
+
 
 const ProductSlider = ({ title }) => {
   var newTitle = "";
@@ -78,47 +53,48 @@ const ProductSlider = ({ title }) => {
           <Loader />
         </div>
       ) : (
-        <Carousel
-          swipeable={true}
-          // centerMode={true}
-          draggable={true}
-          responsive={responsive}
-          className="mt-10"
-          arrows={false}
-          customButtonGroup={<CustomButtonGroup />}
-          additionalTransfrom={0}
-          autoPlaySpeed={2000}
-          centerMode={false}
-          containerClass="container-with-dots"
-          dotListClass=""
-          focusOnSelect={false}
-          infinite
-          itemClass="mr-5"
-          keyBoardControl
-          minimumTouchDrag={80}
-          pauseOnHover
-          renderArrowsWhenDisabled={false}
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots={false}
-          sliderClass=""
-          slidesToSlide={1}
-          autoPlay={true}
+        <Swiper 
+          slidesPerView={3}
+          spaceBetween={30}
+          loop={true}
+          breakpoints={{
+            '@0.00': {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            '@0.75': {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            '@1.00': {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            '@1.50': {
+              slidesPerView: 4,
+              spaceBetween: 50,
+            },
+          }}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Navigation]}
+          navigation={true}
+          className="mySwiper"
         >
           {data.map((item) => {
             return (
-              <Link key={item?.id} className="" to={`/product/${item?.id}`}>
-                <div className="overflow-none productSliderCard">
+              
+                <SwiperSlide>
+                <Link key={item?.id} className="" to={`/product/${item?.id}`}>
                   <Card id={item?.id} data={item} />
-                </div>
-              </Link>
+                  </Link>
+                </SwiperSlide>
+              
             );
           })}
-        </Carousel>
+        </Swiper>
       )}
       <Toaster
         toastOptions={{
